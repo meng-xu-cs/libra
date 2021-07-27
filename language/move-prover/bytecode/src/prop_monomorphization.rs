bcee94a127ee9cb268c546ff28e9057c40cb5290
Meng Xu <mengxu@fb.com>
2021-07-27 10:35:49 -0700

    [move-prover] allow existential type quantifier to proceed

--- language/move-prover/bytecode/src/prop_monomorphization.rs
+++ language/move-prover/bytecode/src/prop_monomorphization.rs
@@ -113,15 +113,12 @@ impl MonoRewriter {
                 }
 
                 // skip mono if this is not a universal quantifier
+                // TODO (mengxu) this check is neither complete nor sound, need to revisit
+                // this when the generic invariant support is ready, i.e., invariant<T, ...>
                 match kind {
                     QuantKind::Forall => (),
                     QuantKind::Exists => {
-                        // TODO (mengxu) this check is neither complete nor sound, need to revisit
-                        // this when the generic invariant support is ready, i.e., invariant<T, ...>
-                        env.error(
-                            &env.get_node_loc(*node_id),
-                            "Existential type quantifiers cannot be locally eliminated",
-                        );
+                        // Existential type quantifiers cannot be locally eliminated
                         return Err(e);
                     }
                     QuantKind::Choose | QuantKind::ChooseMin => {
